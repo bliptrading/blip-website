cbc1578faa0e0fb7e2d6792dbe39d89b3d6a975d
yelpoea <yelpoeayahaya@gmail.com>
2024-09-20 14:30:09 +0000

    Done with the whole ui

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,28 +5,9 @@ const Hero = () => {
   return (
     <div className="w-full flex items-center  bg-center lg:bg-top bg-fixed  bg-my-image h-[250px]">
       {/* <img className="0w-36 ml-40" src="/bplip2.jpeg" /> */}
-      <div className="w-full  lg:hidden flex items-center justify-center ">
+      <div className="w-full px-4  lg:hidden flex items-center justify-center ">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="bg-red-500 p-3 m-1">
-            <FaListUl color={"white"} />
-          </div>
-          <ul
-            tabIndex={0}
-            className="dropdown-content bg-base-100 text-black menu rounded-sm z-[1] w-52 p-2 shadow"
-          >
-            <li>
-              <Link to="/">All</Link>
-            </li>
-            <li>
-              <Link to={"/category/fashion"}>Fashion</Link>
-            </li>
-            <li>
-              <Link to="/category/home-decor">Home Decor</Link>
-            </li>
-            <Link to="/category/electronics">
-              <a>Electronics</a>
-            </Link>
-          </ul>
+         
         </div>
         <input
           type="text"
